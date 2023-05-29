ef481acd8422169d152e6c44b2d446873c317cf7
Daniil600 <ter6062@ya.ru>
2023-05-29 14:49:53 +0300

    How many times will the mother see the ball pass in front of her window

---
+++ src/main/java/org/example/BouncingBalls.java
@@ -0,0 +1,41 @@
+package org.example;
+
+/**
+ * How many times will the mother see the ball pass in front of her window (including when it's falling and bouncing?)
+ * @Param:
+ * @Float parameter "h" in meters must be greater than 0
+ * @Float parameter "bounce" must be greater than 0 and less than 1
+ * @Float parameter "window" must be less than h.
+ * @If all three conditions above are fulfilled, return a positive integer, otherwise return -1.
+ */
+public class BouncingBalls {
+    static int count = 0;
+
+            /**
+             *     This is a method for checking the correctness of the entered data,
+             *      if everything is correct, then it launches the method {@link #calc}
+             * */
+
+    public static int bouncingBall(double h, double bounce, double window) {
+        if (h < window || bounce < 0 || window < 0 || bounce > 1) {
+            return -1;
+        }
+        else {
+            calc(h, bounce, window);
+            return count;
+        }
+    }
+
+    //It's method calculate result
+    private static void calc(double h, double bounce, double window){
+        h = h * bounce;
+        if(h > window){
+            count+=2;
+        }
+        if(h < window){
+            count++;
+            return;
+        }
+        calc(h, bounce, window);
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,18 +13,21 @@ public class Main {
         SumOfOddNumbers sumOfOddNumbers = new SumOfOddNumbers();
         System.out.println(sumOfOddNumbers.rowSumOddNumbers(3));
 
+        System.out.println("==============================");
 
         //Write a function that accepts an array of 10 integers (between 0 and 9),
         // that returns a string of those numbers in the form of a phone number.
         CreatePhoneNumber createPhoneNumber = new CreatePhoneNumber();
         System.out.println(createPhoneNumber.createPhoneNumber(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 1}));
 
+        System.out.println("==============================");
 
         //Write a function that takes an integer as input, and returns the number of bits
         // that are equal to one in the binary representation of that number. You can guarantee that input is non-negative.
         BitCounting bitCounting = new BitCounting();
         System.out.println(bitCounting.countBits(1234));
 
+        System.out.println("==============================");
 
         //Deadfish has 4 commands, each 1 character long:
         //i increments the value (initially 0)
@@ -33,7 +36,9 @@ public class Main {
         //o outputs the value into the return array
         System.out.println(Arrays.toString(DeadfishSwim.parse("iiisdoso")));
 
+        System.out.println("==============================");
 
+        System.out.println(BouncingBalls.bouncingBall(30.0, 0.66, 1.5));
 
     }
 }
